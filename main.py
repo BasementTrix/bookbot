4ad5ff48f769bb91cf48312e1c9cd7a79a79fd6c
Trix Farrar <trix@basement.net>
2025-12-01 22:22:56 -0600

    Book Report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from stats import word_count
+from stats import word_count, character_freq
 
 def get_book_text( file_path ) :
     with open( file_path ) as fp :
@@ -8,10 +8,65 @@ def get_book_text( file_path ) :
 
     return book_text
 
-def main() :
-    text = get_book_text( "books/frankenstein.txt" )
+def report_sort( entry ) :
+    return entry[ "num" ]
+
+def book_report( char_freq ) :
+    report_text = []
+    for key in sorted( char_freq ) :
+        report_text.append( { "char": key,  "num": char_freq[ key ] } )
+        # print( key )
+
+    report_text.sort( key=report_sort, reverse=True )
+
+    for line in report_text :
+        key = line[ "char" ]
+        val = line[ "num" ]
+        print( f"{key}: {val}" )
+# e: 44538
+# t: 29493
+# a: 25894
+# o: 24494
+# i: 23927
+# n: 23643
+# s: 20360
+# r: 20079
+# h: 19176
+# d: 16318
+# l: 12306
+# m: 10206
+# u: 10111
+# c: 9011
+# f: 8451
+# y: 7756
+# w: 7450
+# p: 5952
+# g: 5795
+# b: 4868
+# v: 3737
+# k: 1661
+# x: 691
+# j: 497
+# q: 325
+# z: 235
+# æ: 28
+# â: 8
+# ê: 7
+# ë: 2
+# ô: 1
+
+def main( book="books/frankenstein.txt" ) :
+
+    text = get_book_text( book )
+    letter_counts = character_freq( text )
     
-    print( word_count( text ) )
+    print(  "============ BOOKBOT ============" )
+    print( f"Analyzing book found at {book}..." )
+    print(  "----------- Word Count ----------" )
+    print(  "Found " + str( word_count( text ) ) + " total words" )
+    print(  "--------- Character Count -------" )
+    book_report( letter_counts )
+    print(  "============= END ===============" )
 
 main()
 
--- stats.py
+++ stats.py
@@ -3,3 +3,18 @@ def word_count( input_text ) :
     num_words = len( words )
     return f"Found {num_words} total words"
 
+## def char_sort( body_text ) :
+    
+
+def character_freq( input_text ) :
+    char_freq = {}
+    for i in range( 0, len(input_text) ) :
+        this_char = input_text[ i ]
+        this_char = this_char.lower()
+        if this_char in char_freq :
+            char_freq[ this_char ] += 1
+        else :
+            char_freq[ this_char ] = 1
+    
+    return char_freq 
+
